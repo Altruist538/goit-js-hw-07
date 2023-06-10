0531e5a18963ed3b404a27db8ee61af7d6b68587
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-06-10 11:10:49 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// import * as basicLightbox from "basiclightbox";
+const galleryEl = document.querySelector(".gallery");
+const galleryAdd = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href = "${original}" >
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /> </a >
+    </li>`
+  )
+  .join("");
+galleryEl.innerHTML = galleryAdd;
 
-console.log(galleryItems);
+// galleryEl.addEventListener("click", (event) => {
+//   event.preventDefault();
+
+//   const { target } = event;
+//   if (target.nodeName !== "IMG") return;
+
+//   const largeImageURL = target.dataset.source;
+
+//   const instance = basicLightbox.create(`
+//     <img src="${largeImageURL}" alt="${target.alt}" />
+//   `);
+
+//   instance.show();
+// });
+// console.log(galleryItems);
+galleryEl.addEventListener("click", OnClickGalleryImg);
+
+function OnClickGalleryImg(evt) {
+  evt.preventDefault();
+  const isImgEl = evt.target.classList.contains("gallery__image");
+  if (!isImgEl) {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+  galleryEl.addEventListener("keydown", OnCloswGalleryImg);
+  function OnCloswGalleryImg(evt) {
+    instance.close();
+  }
+}
